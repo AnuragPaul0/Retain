f7c6d135b94d1c95afe33c057ffd987acd7964e7
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-11 20:14:06 +0530

    shadow x

--- app/page.tsx
+++ app/page.tsx
@@ -190,7 +190,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -211,7 +211,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -255,7 +255,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -277,7 +277,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -320,7 +320,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -341,7 +341,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -384,7 +384,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -405,7 +405,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
       <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
         borderBottomLeftRadius: 'var(--nextui-radius-large)'
       }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -447,7 +447,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
         width={200} /> <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
           borderBottomLeftRadius: 'var(--nextui-radius-large)'
         }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
         <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
         <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
         size="sm">
@@ -466,7 +466,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
     width={200} /> <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
       borderBottomLeftRadius: 'var(--nextui-radius-large)'
     }} className=
-  "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+  "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
     <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
     <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
     size="sm">
