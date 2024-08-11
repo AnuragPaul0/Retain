1cf41e3df26a7df3bf80e06497f5feedcab7274c
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-11 20:52:56 +0530

    button

--- app/page.tsx
+++ app/page.tsx
@@ -182,22 +182,19 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
         <Col xs lg="4" className="text-center" style={{fontFamily: 'Recoleta Medium'}}
         >Product Filter</Col>
         <Col xs lg="2" className="justify-content-md-center" style={{display: 'flex'}}>
-        <Card isFooterBlurred radius="lg" className="border-none" >
-      <Image alt="Woman"
-        className="object-cover opacity-1" height={200}
-        src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/6e61b/MainBefore.avif"
-        width={200} />
-      <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
-        borderBottomLeftRadius: 'var(--nextui-radius-large)'
-      }} className=
-      "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
-        <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
-        <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
-        size="sm">
-          Notify
-        </Button>
-      </CardFooter>
-    </Card>
+          <Card isFooterBlurred radius="lg" className="border-none" >
+        <Image alt="Woman"
+          className="object-cover opacity-1" height={200}
+          src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/6e61b/MainBefore.avif"
+          width={200} />
+        <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
+          borderBottomLeftRadius: 'var(--nextui-radius-large)'
+        }} className=
+        "justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
+          <p className="text-tiny" style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p>
+          </CardFooter> </Card>
+          <Button className="text-tiny text-white bg-black/20" variant="flat" color="default" radius="lg"
+        size="sm">Notify</Button>
     {/* <p style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p> */}
     </Col>
 
