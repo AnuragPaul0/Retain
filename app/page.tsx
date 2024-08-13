202f91be64458afd3d79fa71fb4db66167fbf28b
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-13 10:28:01 +0530

    c2

--- app/page.tsx
+++ app/page.tsx
@@ -118,8 +118,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
   <span style={{width: '25%', display: 'inline-block', borderBottom: '1px solid grey'}}>Rules space</span> </p></div>
   
   <button className="button" style={{verticalAlign:'middle', display: 'inline-block',
-  position: 'absolute', top: '3rem',
-  borderRadius: '4px', right: '2rem',
+  position: 'absolute', top: '3rem', borderRadius: '4px', right: '2rem',
   backgroundColor: 'limegreen',
   border: 'none',
   color: '#FFFFFF',
@@ -154,7 +153,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
 
 {/* () => {co = 'red'; () =>{ co = 'white';  */}
     <Row id='r1' onMouseOver={changeBackground }
-      onMouseLeave={changeBackground } className="pb-4 justify-content-md-center">
+      onMouseLeave={changeBackground } className="pb-7 justify-content-md-center">
       <Col xs lg="1"><Row className="justify-content-md-center">
           <Col id='del' className="justify-content-md-center" style={{display: 'flex'}}>
           {/* {changeBackground(isShown)} */}
@@ -173,7 +172,28 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
 <path d="M19 21C20.1046 21 21 20.1046 21 19C21 17.8954 20.1046 17 19 17C17.8954 17 17 17.8954 17 19C17 20.1046 17.8954 21 19 21Z" fill="#000000"/>
             </svg> </Col> </Row> </Col>
       <Col xs lg="4" className="text-center" style={{fontFamily: 'Recoleta Medium'}}
-        >Product Filter</Col>
+        ><Row className="justify-content-md-center">
+          <Col id='del' className="justify-content-md-center" style={{display: 'flex'}}>
+            <Button style={{ top: '50%' }} className=
+              "radius-large absolute text-tiny text-white bg-white/20 z-10" variant="flat" color="default"
+              radius="lg" size="sm">Image list Product Image 2</Button>
+          {/* {changeBackground(isShown)} */}
+          </Col><Col className="justify-content-md-center" style={{display: 'flex'}}>
+            <Button style={{ top: '50%' }} className=
+              "radius-large absolute text-tiny text-white bg-limegreen/20 z-10" variant="flat" color="default"
+              radius="lg" size="sm">is empty</Button></Col></Row>
+        <Row><Col className="justify-content-md-center" style={{display: 'flex'}}>
+          <Button style={{ top: '50%' }} className=
+            "radius-large absolute text-tiny text-white bg-white/20 z-10" variant="flat" color="default"
+            radius="lg" size="sm">and Discount %</Button></Col>
+          <Col className="justify-content-md-center" style={{display: 'flex'}}>
+            <Button style={{ top: '50%' }} className=
+              "radius-large absolute text-tiny text-white bg-limegreen/20 z-10" variant="flat" color="default"
+              radius="lg" size="sm">is</Button></Col>
+          <Col className="justify-content-md-center" style={{display: 'flex'}}>
+            <Button style={{ top: '50%' }} className=
+              "radius-large absolute text-tiny text-white bg-white/20 z-10" variant="flat" color="default"
+              radius="lg" size="sm">0</Button></Col></Row></Col>
       <Col xs lg="2" className="justify-content-md-center" style={{position: 'relative',
         display: 'flex'}}>
         <Card isFooterBlurred radius="lg" className="border-none">
@@ -183,19 +203,19 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
             width={200} />
           <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
             borderBottomLeftRadius: 'var(--nextui-radius-large)' }} className=
-              "justify-centre before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
+              "justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] ml-1 z-10">
               <p className="text-tiny" style={{marginBottom: 0, fontFamily: 'Recoleta Medium'}}
                 >Single Image prduct...</p></CardFooter></Card>
         <Button style={{ top: '50%' }} className=
-            "absolute text-tiny text-white bg-black/20 z-10" variant="flat" color="default"
+            "radius-large absolute text-tiny text-white bg-black/20 z-10" variant="flat" color="default"
           radius="lg" size="sm">
             <svg fill="#000000" height="200px" width="200px" version="1.1"
-          id="Capa_1" viewBox="0 0 348.882 348.882" xmlSpace="preserve">
-            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-        <g id="SVGRepo_iconCarrier"> <g> <path d=
+              id="Capa_1" viewBox="0 0 348.882 348.882" xmlSpace="preserve">
+              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+              <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+              <g id="SVGRepo_iconCarrier"> <g> <path d=
           "M333.988,11.758l-0.42-0.383C325.538,4.04,315.129,0,304.258,0c-12.187,0-23.888,5.159-32.104,14.153L116.803,184.231 c-1.416,1.55-2.49,3.379-3.154,5.37l-18.267,54.762c-2.112,6.331-1.052,13.333,2.835,18.729c3.918,5.438,10.23,8.685,16.886,8.685 c0,0,0.001,0,0.001,0c2.879,0,5.693-0.592,8.362-1.76l52.89-23.138c1.923-0.841,3.648-2.076,5.063-3.626L336.771,73.176 C352.937,55.479,351.69,27.929,333.988,11.758z M130.381,234.247l10.719-32.134l0.904-0.99l20.316,18.556l-0.904,0.99 L130.381,234.247z M314.621,52.943L182.553,197.53l-20.316-18.556L294.305,34.386c2.583-2.828,6.118-4.386,9.954-4.386 c3.365,0,6.588,1.252,9.082,3.53l0.419,0.383C319.244,38.922,319.63,47.459,314.621,52.943z"></path> <path d="M303.85,138.388c-8.284,0-15,6.716-15,15v127.347c0,21.034-17.113,38.147-38.147,38.147H68.904 c-21.035,0-38.147-17.113-38.147-38.147V100.413c0-21.034,17.113-38.147,38.147-38.147h131.587c8.284,0,15-6.716,15-15 s-6.716-15-15-15H68.904c-37.577,0-68.147,30.571-68.147,68.147v180.321c0,37.576,30.571,68.147,68.147,68.147h181.798 c37.576,0,68.147-30.571,68.147-68.147V153.388C318.85,145.104,312.134,138.388,303.85,138.388z"
-          ></path> </g> </g></svg></Button>
+                ></path> </g> </g></svg></Button>
   {/* <p style={{fontFamily: 'Recoleta Medium'}}>Single Image prduct...</p> */}
       </Col>
 
@@ -206,8 +226,7 @@ d="m82.35,31.23c-12.27,0-22.69,8.61-31.41,21.78-12.33,18.61-19.88,46.33-19.88,72
           src="https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/6e61b/MainBefore.avif"
           width={200} />
         <CardFooter style={{borderBottomRightRadius: 'var(--nextui-radius-large)',
-          borderBottomLeftRadius: 'var(--nextui-radius-large)'
-        }} className=
+          borderBottomLeftRadius: 'var(--nextui-radius-large)' }} className=
           "justify-center before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 before:rounded-xl bottom-1 w-[calc(100%_-_8px)] z-10"
           ><p className="text-tiny" style={{marginBottom: 0, fontFamily: 'Recoleta Medium'}}
             >Single Image prduct...</p>
