b1332c4d93b11536f88754fd9ca70795b204e5da
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-18 20:53:11 +0530

    c0

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,9 @@ export default function Home() { let tog, parent, p = 6, container, a, k:any, b:
 
   let relm = (rel:any, c='') => { container = document.createElement('div')
     if (c != '') container.className+=' '+c
-  createRoot(container).render(rel); console.log('c', container); return container.children[0] }
+  createRoot(container).render(rel); console.log('c', container, container.children,
+    container.children[0])
+  return container.children[0] }
 
 
   let greeting = (el:any) => { f = el.id == 'btnr'
