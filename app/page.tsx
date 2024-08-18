dc8313ab6999b6cec63456b057886f76ea41c689
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-18 13:26:22 +0530

    log c

--- app/page.tsx
+++ app/page.tsx
@@ -100,7 +100,7 @@ export default function Home() { let tog, parent, p = 6, container, a, k:any, b:
     if (!f) { tr = document.querySelector('#rt.r')
       if (tr) { node = tr.lastChild; console.log(node)
       clone = node.cloneNode(true)
-      // console.log(clone)
+      console.log(clone)
       clone!.children[0].innerHTML.replace(/.$/, +clone.children[0].innerHTML.slice(-1)+1)
 
       tr.appendChild(clone) }
