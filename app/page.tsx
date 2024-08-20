0e95e19450045d1cf852edc7a29182a1273cd5d8
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-20 14:12:58 +0530

    instanceof HTMLElement

--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function Home() { let tog, parent, p = 6, container, a, k:any, b:
                 "shadow-medium p-1 radius-large text-black"
                 variant="flat" color="default">+ Add Product Filters</Button></div>
       </Card></Card></Col></Row> : c; b = 'beforebegin'
-    if (!f) { a = document.querySelector('#scrw')
+    if (!f) { a = document.querySelector('#scrw') instanceof HTMLElement
       if (a) a.style.width = +a.style.width.slice(0, -2)+15+'vh'
       tr = document.querySelector('#rt.r')
       // if (tr) { node = tr.lastChild
