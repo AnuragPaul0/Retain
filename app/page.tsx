774719c12b8b38c754cabfc6b9231eceb7a47127
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-21 14:55:51 +0530

    {

--- app/page.tsx
+++ app/page.tsx
@@ -1031,7 +1031,7 @@ paddingRight: 'calc(var(--bs-gutter-x) /4) !important' }}>+</Button>
           id="path1961" cx="49.980141" cy="49.922749" rx="20.786863" ry="20.786861"/>
           <ellipse id="path1963" cx="49.980141" cy="49.922749" rx="33.786863" ry="33.786863"/>
           <ellipse id="path1965" cx="49.980141" cy="49.922749" rx="46.786863" ry="46.786863"/>
-      </g></svg></div><span onClick="myFunction" className="close">&times;</span>
+      </g></svg></div><span onClick={myFunction} className="close">&times;</span>
       <p>Selecta design to link</p><nav class="navbar navbar-light bg-light">
     <form class="container-fluid">
       <div class="input-group">
