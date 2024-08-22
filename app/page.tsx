dbdfbd275002ac9f45f6819b4348c34a3a084ed0
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-22 14:28:30 +0530

    a

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,12 @@ export default function Home() { let tog, parent, p = 6, container, a:any, k:any
       { 'r'+o.toString()+' z-10 shadow-medium self-center p-2 radius-large absolute bg-white/20 '}
   variant="flat" color="default" radius="lg" size="sm">+ Add design</Button></Col>
   
-  // window.addEventListener("load", () => {
-    document.querySelector('#scrw > #r1').addEventListener("mouseenter",
-      (e) => changeBackground(e, 'h'))!
+  window.addEventListener("load", () => {
+    a = document.querySelector('#scrw > #r1').addEventListener("mouseenter",
+      (e) => changeBackground(e, 'h'))
     document.querySelector('#scrw > #r1').addEventListener("mouseleave",
   (e) =>changeBackground(e, 'h', 1))
-// })
+})
 
   // const [isShown, setIsShown] = useState(false), co = true,clone:any, cl, node:any, du
   // console.log(c)
