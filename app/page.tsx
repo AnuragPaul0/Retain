3fe07102a80359ce3bb002b1aea3f07fd5045ba7
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-22 11:48:54 +0530

    sdel

--- app/page.tsx
+++ app/page.tsx
@@ -252,8 +252,8 @@ export default function Home() { let tog, parent, p = 6, container, a:any, k:any
             className="r1 self-center" style={{ display: "none",
               paddingLeft: 'calc(var(--bs-gutter-x) /4) !important', borderRadius: '2px',
         paddingRight: 'calc(var(--bs-gutter-x) /4) !important' }}>
-          <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
-            style="color:red;stroke-width:0.5" width="20"><g id="Trash"><g><path d=
+          <svg id='sdel' stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24"
+            width="20"><g id="Trash"><g><path d=
   "M19.45,4.06H15.27v-.5a1.5,1.5,0,0,0-1.5-1.5H10.23a1.5,1.5,0,0,0-1.5,1.5v.5H4.55a.5.5,0,0,0,0,1h.72l.42,14.45a2.493,2.493,0,0,0,2.5,2.43h7.62a2.493,2.493,0,0,0,2.5-2.43l.42-14.45h.72A.5.5,0,0,0,19.45,4.06Zm-9.72-.5a.5.5,0,0,1,.5-.5h3.54a.5.5,0,0,1,.5.5v.5H9.73Zm7.58,15.92a1.5,1.5,0,0,1-1.5,1.46H8.19a1.5,1.5,0,0,1-1.5-1.46L6.26,5.06H17.74Z"
             ></path><path d="M8.375,8h0a.5.5,0,0,1,1,0l.25,10a.5.5,0,0,1-1,0Z"></path><path d=
           "M15.625,8.007a.5.5,0,0,0-1,0h0l-.25,10a.5.5,0,0,0,1,0Z"></path></g></g></svg>
@@ -593,15 +593,13 @@ export default function Home() { let tog, parent, p = 6, container, a:any, k:any
             "M333.988,11.758l-0.42-0.383C325.538,4.04,315.129,0,304.258,0c-12.187,0-23.888,5.159-32.104,14.153L116.803,184.231 c-1.416,1.55-2.49,3.379-3.154,5.37l-18.267,54.762c-2.112,6.331-1.052,13.333,2.835,18.729c3.918,5.438,10.23,8.685,16.886,8.685 c0,0,0.001,0,0.001,0c2.879,0,5.693-0.592,8.362-1.76l52.89-23.138c1.923-0.841,3.648-2.076,5.063-3.626L336.771,73.176 C352.937,55.479,351.69,27.929,333.988,11.758z M130.381,234.247l10.719-32.134l0.904-0.99l20.316,18.556l-0.904,0.99 L130.381,234.247z M314.621,52.943L182.553,197.53l-20.316-18.556L294.305,34.386c2.583-2.828,6.118-4.386,9.954-4.386 c3.365,0,6.588,1.252,9.082,3.53l0.419,0.383C319.244,38.922,319.63,47.459,314.621,52.943z"></path> <path d="M303.85,138.388c-8.284,0-15,6.716-15,15v127.347c0,21.034-17.113,38.147-38.147,38.147H68.904 c-21.035,0-38.147-17.113-38.147-38.147V100.413c0-21.034,17.113-38.147,38.147-38.147h131.587c8.284,0,15-6.716,15-15 s-6.716-15-15-15H68.904c-37.577,0-68.147,30.571-68.147,68.147v180.321c0,37.576,30.571,68.147,68.147,68.147h181.798 c37.576,0,68.147-30.571,68.147-68.147V153.388C318.85,145.104,312.134,138.388,303.85,138.388z"
         ></path></g></g></svg></Button></Col>
 {/* paddingLeft: 'calc(var(--bs-gutter-x) * .8)' */}
-        <Col id="addc" xs lg="1" style={{ width: '5vw', display: 'flex', borderLeft: '1px solid #ddd'
-          }}><Button onClick={greeting} className="shadow-medium self-center" style={{
-              zoom: '2', fontFamily: 'Recoleta Medium',
-              paddingLeft: 'calc(var(--bs-gutter-x) /4) !important',
-              borderRadius: '2px',
+        <Button id="addc" onClick={greeting} className="shadow-medium self-center" style={{
+              zoom: '2', fontFamily: 'Recoleta Medium', width: 'fit-content', marginLeft: '1vw',
+              paddingLeft: 'calc(var(--bs-gutter-x) /4) !important', borderRadius: '2px',
               // transition: 'all 0.5s', cursor: 'pointer'
       paddingRight: 'calc(var(--bs-gutter-x) /4) !important' }}>+</Button>
-  {/* <Row className="justify-content-md-center" style={{ display: 'grid', alignSelf: 'center' }}></Row> */}
-          </Col></Row>
+  {/* <Col xs lg="1" style={{ width: '5vw', display: 'flex', borderLeft: '1px solid #ddd' }}></Col> */}
+      </Row>
 
 
       <Row id='r2' style={{ justifyContent: 'flex-start !important' }}
