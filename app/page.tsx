ff3a54e7246e9aac083f41efc3535af0e2674b12
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-24 12:11:22 +0530

    ar p

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,9 @@ export default function Home() { const [inputValue, setInputValue] = React.useSt
 
   divcar = (e: any) => <div style={{ alignSelf: 'center',
     marginBottom: 'calc(var(--nextui-radius-large) / 2)' }}
-  >{ e.forEach((element) => element) }</div>,
+  >{ Array.prototype.forEach.call(e, child => child)
+    // e.forEach( (element) => element)
+     }</div>,
 // r el
   relem = (i: any, isDragged: any, ele: any) => <Row id={'r'+i} key="input" value={inputValue}
     onChange={(e) => { setInputValue((e.target as HTMLInputElement).value) }} onMouseEnter={
