62700497b93f7d1ce36fe8676b740514b1e7f7a8
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-25 14:53:56 +0530

    ,

--- app/page.tsx
+++ app/page.tsx
@@ -89,7 +89,7 @@ export default function Home() {
   <div key='2' id='alsc'>{buttonc('tags') } {buttonc('contain', greens)} {buttonc('__labelNew')}</div>,
      <div key='3' id='alsc'>{buttonc('Discount %') } {buttonc('is', greens)} {buttonc(0)}</div>,
      <div key='4' id='alsc'>{buttonc('Image list Product Image 2') }{ buttonc('is', greens)}
-  {buttonc('empty')}</div> ]
+  {buttonc('empty')}</div> ],
   // console.log(el2)let
 
   els = [ <Card key='0' className="w90 justify-content-md-center"><div id='alsc'
