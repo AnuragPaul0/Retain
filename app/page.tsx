28e30fb5177fda9f149bc9ad8f590b4f4d86d28b
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-25 13:37:47 +0530

    v

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const RemovableIcon = () =>
     stroke-width="0" viewBox="0 0 24 24" width="20"><title>Remove</title><g id="Trash"><g><path d=
 "M19.45,4.06H15.27v-.5a1.5,1.5,0,0,0-1.5-1.5H10.23a1.5,1.5,0,0,0-1.5,1.5v.5H4.55a.5.5,0,0,0,0,1h.72l.42,14.45a2.493,2.493,0,0,0,2.5,2.43h7.62a2.493,2.493,0,0,0,2.5-2.43l.42-14.45h.72A.5.5,0,0,0,19.45,4.06Zm-9.72-.5a.5.5,0,0,1,.5-.5h3.54a.5.5,0,0,1,.5.5v.5H9.73Zm7.58,15.92a1.5,1.5,0,0,1-1.5,1.46H8.19a1.5,1.5,0,0,1-1.5-1.46L6.26,5.06H17.74Z"
     ></path><path d="M8.375,8h0a.5.5,0,0,1,1,0l.25,10a.5.5,0,0,1-1,0Z"></path><path d=
-  "M15.625,8.007a.5.5,0,0,0-1,0h0l-.25,10a.5.5,0,0,0,1,0Z"></path></g></g></svg>,
+"M15.625,8.007a.5.5,0,0,0-1,0h0l-.25,10a.5.5,0,0,0,1,0Z"></path></g></g></svg>,
 
 HandleIcon = <svg height="20px" className="feather feather-move" viewBox="0 0 24 24"
   fill="none"><path d=
@@ -44,23 +44,14 @@ buttonc = (t: any, stru = {}) => <Button style={{
   marginRight: 'calc(var(--nextui-radius-large) / 2)', ...stru }} className=
 "shadow-medium p-1 text-black" variant="flat" color="default">{ t }</Button>,
 
-greens = { color: '#052', backgroundColor: '#04ae5620' },
+greens = { color: '#052', backgroundColor: '#04ae5620' }
 
   // divcar = (e: any) => <div style={{ alignSelf: 'center', key="input"value={inputValue}
     // onChange={(e) => { setInputValue((e.target as HTMLInputElement).value) }}
-  //   marginBottom: 'calc(var(--nextui-radius-large) / 2)' }}
-  // >{ [...e].forEach( (element:any) => element )
-  //   // Array.prototype.forEach.call(e, child => child)
-  //    }</div>,
-// r el
-  {/* <Button data-movable-handle  style={{ border: "none",
-  margin: 0, width: "auto", overflow: "visible",
-  background: "transparent", cursor: isDragged ? "grabbing" : "grab",
-  marginRight: "3em", padding: 0,
-  borderRadius: '2px' }} tabIndex={-1}>{ HandleIcon }</Button> */}
 
 
 export default function Home() { 
+  // r el
   const relem = (i: any, isDragged: any, c: any, index: any) => {
     // console.log(c)
     return <Row id={'r'+i}  onMouseEnter={ (e) => changeBackground(e, 'h') }
