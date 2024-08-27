e1e29dbf6079dff5636887318753bbcaabeb0eff
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-08-27 10:36:03 +0530

    t

--- app/page.tsx
+++ app/page.tsx
@@ -1180,7 +1180,7 @@ export default function Home() {
           "M333.988,11.758l-0.42-0.383C325.538,4.04,315.129,0,304.258,0c-12.187,0-23.888,5.159-32.104,14.153L116.803,184.231 c-1.416,1.55-2.49,3.379-3.154,5.37l-18.267,54.762c-2.112,6.331-1.052,13.333,2.835,18.729c3.918,5.438,10.23,8.685,16.886,8.685 c0,0,0.001,0,0.001,0c2.879,0,5.693-0.592,8.362-1.76l52.89-23.138c1.923-0.841,3.648-2.076,5.063-3.626L336.771,73.176 C352.937,55.479,351.69,27.929,333.988,11.758z M130.381,234.247l10.719-32.134l0.904-0.99l20.316,18.556l-0.904,0.99 L130.381,234.247z M314.621,52.943L182.553,197.53l-20.316-18.556L294.305,34.386c2.583-2.828,6.118-4.386,9.954-4.386 c3.365,0,6.588,1.252,9.082,3.53l0.419,0.383C319.244,38.922,319.63,47.459,314.621,52.943z"></path> <path d="M303.85,138.388c-8.284,0-15,6.716-15,15v127.347c0,21.034-17.113,38.147-38.147,38.147H68.904 c-21.035,0-38.147-17.113-38.147-38.147V100.413c0-21.034,17.113-38.147,38.147-38.147h131.587c8.284,0,15-6.716,15-15 s-6.716-15-15-15H68.904c-37.577,0-68.147,30.571-68.147,68.147v180.321c0,37.576,30.571,68.147,68.147,68.147h181.798 c37.576,0,68.147-30.571,68.147-68.147V153.388C318.85,145.104,312.134,138.388,303.85,138.388z"
     ></path></g></g></svg></Button></Col></Row></Container>
 
-    <Container id='lis' style={{ maxWidth: '88vw' }} className="cnr rounded-large">
+    <ScrollSync><Container id='lis' style={{ maxWidth: '88vw' }} className="cnr rounded-large">
 
       <ul id='mb0' className="pl0"><li style={{ padding: "0 1.5em", listStyleType: "none" }}>
         <Row id='rt' style={{ marginBottom: 'calc(2 * var(--bs-gutter-x))' }}>
@@ -1193,7 +1193,7 @@ export default function Home() {
                 paddingRight: '3% !important' }}
         isFooterBlurred radius="lg" className="cb p-1 border-none">Product Filter</Card></Col>
 
-        <div style={{ width: '50vw', overflow: 'auto' }}>
+      <ScrollSyncPane><div style={{ width: '50vw', overflow: 'auto' }}>
             <Row id='scrw' className='r' style={{ width: '35vw' }}>
               <Col xs lg="2" className="w3 justify-content-md-center" style={{
                 display: 'flex' }}><Card style={{ flexGrow: 1, boxShadow: 'none',
@@ -1221,13 +1221,13 @@ export default function Home() {
                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 className="lucide lucide-ellipsis-vertical">
                   <circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/>
-      </svg></Button></Col></Row></div></Row></li></ul>
+      </svg></Button></Col></Row></div></ScrollSyncPane></Row></li></ul>
 
         <List values={items} onChange={({ oldIndex, newIndex }) =>
           setItems(arrayMove(items, oldIndex, newIndex)) }
-        renderList={({ children, props, isDragged }) => <ScrollSync><ul
+        renderList={({ children, props, isDragged }) => <ul
             {...props} style={{ padding: "0em 0em 1em 0em",
-              cursor: isDragged ? "grabbing" : "inherit" }}>{children}</ul></ScrollSync> }
+              cursor: isDragged ? "grabbing" : "inherit" }}>{children}</ul> }
 
         renderItem={({ value, props, index, isDragged, isSelected }) => {
           // console.log(value)
@@ -1252,7 +1252,7 @@ export default function Home() {
     <Col style={{ width: '67%' }} xs lg="4"></Col></Row></li></ul>
 
     {/* greeting <button onClick={ }>Reset</button> */}
-    </Container>
+    </Container></ScrollSync>
 
     <div id="imodal" className="modal">
       <div className="modal-content" id="imodal-content" style={{ justifyContent: 'center' }} >
