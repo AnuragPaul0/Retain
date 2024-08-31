e15326d57d0c7491e699d390db7e5a27bce0a237
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-01 00:10:15 +0530

    cl, k

--- app/page.tsx
+++ app/page.tsx
@@ -280,6 +280,7 @@ cfo = [[sing, sing], ['Multi Image - '+os, '4 Images - '+os],
 ['Single Image prduct', 'Single Image prduct']],
 
 btn = (r=0, c=0) => <Button id='b1' onClick={ (e) => { let cl=(e.target as HTMLElement).classList
+  consol.log(cl, k)
   for (let i = 0; i < cl.length; i++) { if (cl[i].match(/m\d/)) {
     document.querySelector('.'+k)?.replaceWith(relm(imco(+cl[i][1], +cl[i][2]?iml:dict))
     // '.'+cl[i].substring(1))
