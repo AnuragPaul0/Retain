04a27d23c9f6b10b2b4d864fac782e7eff41fbeb
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-04 16:10:44 +0530

    x

--- app/one/page.tsx
+++ app/one/page.tsx
@@ -318,10 +318,10 @@ export default function Home(){
       // console.log(b, relm(k))
       setName([...name, {id: co}]); co++; setArtists(arts); tr = document.querySelector("#topdo")
       tr?.classList.add('df')
-      let tem = [document.querySelector(".fon.p-2"), document.querySelector("rect")]
-      tem[0]?.classList.add('modal-cont'), tem[1]?.setAttribute('id', 'wed')
-      setTimeout(() => { tr?.classList.remove('df'); tem[0]?.classList.remove('modal-cont')
-        tem[1]?.removeAttribute("id");
+      // let tem = [document.querySelector(".fon.p-2"), document.querySelector("rect")]
+      // tem[0]?.classList.add('modal-cont'), tem[1]?.setAttribute('id', 'wed')
+      setTimeout(() => { tr?.classList.remove('df')
+        // tem[0]?.classList.remove('modal-cont') tem[1]?.removeAttribute("id");
        }, 1000)
       // console.log(artists)
     } } className={ 'r'+i+" fon shadow-medium self-center" }>+</Button></Row>,
@@ -605,7 +605,7 @@ export default function Home(){
         <circle r="150" cx="150" cy="150" fill="green"/>
         <path d="M152.502,0.001C68.412,0.001,0,68.412,0,152.501s68.412,152.5,152.502,152.5c84.089,0,152.5-68.411,152.5-152.5    S236.591,0.001,152.502,0.001z M152.502,280.001C82.197,280.001,25,222.806,25,152.501c0-70.304,57.197-127.5,127.502-127.5    c70.304,0,127.5,57.196,127.5,127.5C280.002,222.806,222.806,280.001,152.502,280.001z"/>
         <path d="M218.473,93.97l-90.546,90.547l-41.398-41.398c-4.882-4.881-12.796-4.881-17.678,0c-4.881,4.882-4.881,12.796,0,17.678    l50.237,50.237c2.441,2.44,5.64,3.661,8.839,3.661c3.199,0,6.398-1.221,8.839-3.661l99.385-99.385    c4.881-4.882,4.881-12.796,0-17.678C231.269,89.089,223.354,89.089,218.473,93.97z"/>
-        <rect height="150" y="80" rx="20" ry="20" fill="green"/>
+        <rect id='wid' height="150" y="80" rx="20" ry="20" fill="green"/>
     </svg>added</div></div>
 
     <div id="imodal" className="modal modalp">
