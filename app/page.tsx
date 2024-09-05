9c22ceb6a4186d5c69e951085a203b9740c9efaf
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-06 01:22:26 +0530

    i2

--- app/page.tsx
+++ app/page.tsx
@@ -600,7 +600,7 @@ export default function Home(){
       <nav aria-label="Vercel Directory"><div className="statusRow">
         <fieldset className="tailwind root" data-small="">
           <legend className="sr-only">Select a display theme:</legend><span>
-            <input aria-label="system" id="theme-switch-system" type="radio" value="system"
+            {/* <input aria-label="system" id="theme-switch-system" type="radio" value="system"
             name="the" defaultChecked={true}/><label htmlFor="theme-switch-system">
               <span className="sr-only">system</span>
               <svg data-testid="geist-icon" height="16" stroke-linejoin="round"
@@ -632,7 +632,7 @@ export default function Home(){
               <svg data-testid="geist-icon" height="16" stroke-linejoin="round"
                 viewBox="0 0 16 16" width="16">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 3.25C0.5 1.45507 1.95507 0 3.75 0H8.25C10.0449 0 11.5 1.45507 11.5 3.25V11.25V12H10.75H1.25H0.5V11.25V3.25ZM3.75 1.5C2.7835 1.5 2 2.2835 2 3.25V10.5H10V3.25C10 2.2835 9.2165 1.5 8.25 1.5H3.75ZM3 3.5C3 2.94772 3.44772 2.5 4 2.5H8C8.55228 2.5 9 2.94772 9 3.5V7H3V3.5ZM6.5 9.5H9V8H6.5V9.5Z"
-              fill="currentColor" transform="translate(2.5, 2)"></path></svg></label></span><span>
+              fill="currentColor" transform="translate(2.5, 2)"></path></svg></label></span><span> */}
             <input
             name="state"
             aria-label="light" id="theme-switch-light-:R2jcj9am:" type="radio"
