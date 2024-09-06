0fec1cda72a1333130181e9cb40d2bd171d8201d
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-06 11:40:53 +0530

    trash

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { ScrollSync, ScrollSyncPane } from 'react-scroll-sync'
 import { List, arrayMove, arrayRemove } from "react-movable"
 import { useState } from "react"
-import { CiTrash } from "react-icons/ci"
+// import { CiTrash } from "react-icons/ci"
 import {  MdOutlineSettings } from "react-icons/md"
 import { HiArrowLeft } from "react-icons/hi"
 import { Row, Col, Container } from 'react-bootstrap'
@@ -104,7 +104,13 @@ greeting = (el:any) => { let tt = el.target; f = tt.id == 'btnr'
           style={{ display: "none", zoom: '2', fontFamily: 'Recoleta Medium',
             paddingLeft: 'calc(var(--bs-gutter-x) /4) !important', borderRadius: '2px',
       paddingRight: 'calc(var(--bs-gutter-x) /4) !important' }}>
-        <CiTrash height='20' style={{ color: 'red', strokeWidth: .5}}/></Button></Col></Row>
+        {/* <CiTrash height='20' style={{ color: '', strokeWidth: .5}}/> */}
+        <svg stroke="red" fill="currentColor" stroke-width=".5" viewBox="0 0 24 24" height="20px"
+          ><g id="Trash"><g><path d=
+            "M19.45,4.06H15.27v-.5a1.5,1.5,0,0,0-1.5-1.5H10.23a1.5,1.5,0,0,0-1.5,1.5v.5H4.55a.5.5,0,0,0,0,1h.72l.42,14.45a2.493,2.493,0,0,0,2.5,2.43h7.62a2.493,2.493,0,0,0,2.5-2.43l.42-14.45h.72A.5.5,0,0,0,19.45,4.06Zm-9.72-.5a.5.5,0,0,1,.5-.5h3.54a.5.5,0,0,1,.5.5v.5H9.73Zm7.58,15.92a1.5,1.5,0,0,1-1.5,1.46H8.19a1.5,1.5,0,0,1-1.5-1.46L6.26,5.06H17.74Z"
+            ></path><path d="M8.375,8h0a.5.5,0,0,1,1,0l.25,10a.5.5,0,0,1-1,0Z"></path><path d=
+            "M15.625,8.007a.5.5,0,0,0-1,0h0l-.25,10a.5.5,0,0,0,1,0Z"></path></g></g></svg></Button>
+      </Col></Row>
       <Row style={{ flexGrow:.5 }}><Col id='req' className="justify-content-md-center"
         style={{ blockSize: 'fit-content', display: 'flex' }}>
         <p className='p5' style={{ marginBottom: 0, zoom: '2', fontFamily: 'Recoleta Medium' }}
