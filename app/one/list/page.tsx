1e5b3ab37a1571b73a954e07b37e7a286b4fbed6
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-07 11:30:39 +0530

    l

--- app/one/list/page.tsx
+++ app/one/list/page.tsx
@@ -6,12 +6,7 @@ export default function Home() {
   const [items, setItems] = React.useState(["Item 1", "Item 2", "Item 3"])
   return (
     <List
-      values={items}
-      onChange={({ oldIndex, newIndex }) =>
-        setItems(arrayMove(items, oldIndex, newIndex))
-      }
-      renderList={({ children, props }) => <ul {...props}>{children}</ul>}
-      renderItem={({ value, props }) => <li {...props}>{value}</li>}
+      
     />
   )
 }
