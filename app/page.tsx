f17e54e1325969fc29d59008311b7feb88b0d5e3
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-08 23:24:11 +0530

    .currentTarget

--- app/page.tsx
+++ app/page.tsx
@@ -536,7 +536,8 @@ export default function Home(){
         >Design for remaining SKU's</span><Switch color="success" defaultSelected
     onValueChange={setIsSelected} aria-label="Automatic updates"/>
     <label onClick={ (e) => { let el = e.target as HTMLElement, dat = 'data-selected'
-      el.hasAttribute(dat) ? el.removeAttribute(dat) : el.setAttribute(dat, "true") } }
+      if (el == e.currentTarget) el.hasAttribute(dat) ? el.removeAttribute(dat) : el.setAttribute(dat,
+        "true") } }
       style={{  marginLeft: '6rem' }} aria-label="Automatic updates" className="group relative max-w-fit inline-flex items-center justify-start cursor-pointer touch-none tap-highlight-transparent"
       data-selected="true"><span id='spa'>
         <input aria-labelledby=":R4mfja:" type="checkbox" role="switch" defaultChecked={true} value=""/>
