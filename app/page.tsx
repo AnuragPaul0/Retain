231e918121ddbc95d8c7518d8eb4b583224c375c
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-09 09:01:49 +0530

    dc x

--- app/page.tsx
+++ app/page.tsx
@@ -537,32 +537,17 @@ export default function Home(){
         <Switch color="success" defaultSelected
     onValueChange={setIsSelected} aria-label="Automatic updates"/>
     <label onMouseUp={ (e) => { console.log('el', e.target)
-        let el = e.currentTarget as HTMLElement, dat = 'data-pressed'
-        // console.log(el)
-      // if ((e.target as HTMLElement).tagName == 'INPUT') el.hasAttribute(dat) ?
-      // el.setAttribute(dat, "true") :
-        el.removeAttribute(dat)
+        let el = e.currentTarget as HTMLElement, dat = 'data-pressed'; el.removeAttribute(dat)
          }} onMouseDown={ (e) => { console.log('el', e.target)
         let el = e.currentTarget as HTMLElement, dat = 'data-pressed'
-        // console.log(el)if () el.hasAttribute(dat) ? (e.target as HTMLElement).tagName == 'INPUT'
-      // el.removeAttribute(dat) :
         el.setAttribute(dat, "true") }} onClick={ (e) => { console.log('el', e.target)
         let el = e.currentTarget as HTMLElement, dat = 'data-selected'
-        // console.log(el)
+        // console.log(el) defaultChecked={true}
       if ((e.target as HTMLElement).tagName == 'INPUT') el.hasAttribute(dat) ? el.removeAttribute(dat) :
         el.setAttribute(dat, "true") } }
       style={{  marginLeft: '6rem' }} aria-label="Automatic updates" className="group relative max-w-fit inline-flex items-center justify-start cursor-pointer touch-none tap-highlight-transparent"
       ><span id='spa'>
-        <input aria-labelledby=":R4mfja:" type="checkbox" role="switch" defaultChecked={true}/>
-      </span>
-      <span aria-hidden="true" className="px-1 relative inline-flex items-center justify-start flex-shrink-0 overflow-hidden bg-default-200 rounded-full outline-none group-data-[focus-visible=true]:z-10 group-data-[focus-visible=true]:ring-2 group-data-[focus-visible=true]:ring-focus group-data-[focus-visible=true]:ring-offset-2 group-data-[focus-visible=true]:ring-offset-background group-data-[selected=true]:bg-success group-data-[selected=true]:text-success-foreground w-12 h-7 mr-2 rtl:ml-2 rtl:mr-[unset] transition-background">
-        <span id='swi' className="z-10 flex items-center justify-center bg-black shadow-small rounded-full origin-right w-5 h-5 text-small group-data-[selected=true]:ml-5 rtl:group-data-[selected=true]:ml-0 rtl:group-data-[selected=true]:mr-5 transition-all group-data-[pressed=true]:w-6 group-data-[selected]:group-data-[pressed]:ml-4">
-    </span></span></label>
-    
-    <label
-      style={{  marginLeft: '6rem' }} aria-label="Automatic updates" className="group relative max-w-fit inline-flex items-center justify-start cursor-pointer touch-none tap-highlight-transparent"
-      data-selected="true"><span id='spa'>
-        <input aria-labelledby=":R4mfja:" type="checkbox" role="switch" defaultChecked={true} value=""/>
+        <input aria-labelledby=":R4mfja:" type="checkbox" role="switch"/>
       </span>
       <span aria-hidden="true" className="px-1 relative inline-flex items-center justify-start flex-shrink-0 overflow-hidden bg-default-200 rounded-full outline-none group-data-[focus-visible=true]:z-10 group-data-[focus-visible=true]:ring-2 group-data-[focus-visible=true]:ring-focus group-data-[focus-visible=true]:ring-offset-2 group-data-[focus-visible=true]:ring-offset-background group-data-[selected=true]:bg-success group-data-[selected=true]:text-success-foreground w-12 h-7 mr-2 rtl:ml-2 rtl:mr-[unset] transition-background">
         <span id='swi' className="z-10 flex items-center justify-center bg-black shadow-small rounded-full origin-right w-5 h-5 text-small group-data-[selected=true]:ml-5 rtl:group-data-[selected=true]:ml-0 rtl:group-data-[selected=true]:mr-5 transition-all group-data-[pressed=true]:w-6 group-data-[selected]:group-data-[pressed]:ml-4">
