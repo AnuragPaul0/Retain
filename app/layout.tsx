1b926850a0d6a2070e135d7d34ea57878ba89849
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-10 22:27:35 +0530

    dark

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,12 +12,12 @@ icon: "https://framerusercontent.com/images/E4ASOtv32Ln0x5c9sOJ8aUHtQYg.png"} }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) { return (
   <html lang="en"><body><script dangerouslySetInnerHTML={{ __html: `
-                // console.log(document.readyState)
+    // console.log(document.readyState)
     d=document.documentElement,c=d.classList;
-      c.remove('light-theme','dark-theme');var e=localStorage.getItem('mode')
-      if('system'===e||(!e&&true)){var t='(prefers-color-scheme: dark)',m=window.matchMedia(t)
-      if(m.media!==t||m.matches){ c.add('dark')}else{
-        d.style.colorScheme = 'light';c.add('light-theme')}}else if(e){
-          var x={"light":"light-theme","dark":"dark-theme"};c.add(x[e]|| '')}
+    c.remove('light-theme','dark-theme');var e=localStorage.getItem('mode')
+    if('system'===e||(!e&&true)){var t='(prefers-color-scheme: dark)',m=window.matchMedia(t)
+      if(m.media!==t||m.matches) c.add('dark') else {
+      d.style.colorScheme = 'light';c.add('light-theme')}}else if(e){
+    var x={"light":"light-theme","dark":"dark-theme"};c.add(x[e]|| '')}
           if(e==='light'||e==='dark')d.style.colorScheme=e`,
 }}></script>{children}</body></html> ) }
