6935e856655d794d22090a8188aeef8802a5a79e
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-13 18:21:17 +0530

    d

--- app/page.tsx
+++ app/page.tsx
@@ -225,27 +225,6 @@ export default function Home() {
 
   [artists, setArtists] = useState(arr), [name, setName] = useState([{ id: 1 }]),
   [isSelected, setIsSelected] = useState(true),
-// className={" mwu self-center"}
-      //   onClick={ (e:any) => { let arts = artists, c = (e.target as HTMLElement).classList, inde=0;
-      //     nextId-=15
-      //     for (let i = 0; i < c.length; i++) { if (c[i].match(/c\d/)) {
-      //       // console.log(c[i])
-      //     inde = +c[i].substring(1); break } }
-      // artists.forEach((e:any) => { e.splice(inde, 1) // remove 1 item only
-      // } ); tr = document.querySelector('#scrw.r')
-      // // Variant
-      // tr?.append(relm(<Col xs lg="2" className="df justify-content-md-center"
-      //   style={{ width: '100%' }}>
-      //     <Card id='fg1' isFooterBlurred radius="lg" className="br2 cb p-1 border-none"
-      // >Variant { co+1 }</Card></Col>, ['w3', 'df', 'top'] ) )
-      // // console.log(b, relm(k))"c"+{ind}+
-      // setArtists(arts); todo('Variant', 'removed!') } }
-      //   <svg id='sdel' className="feather feather-x-circle" stroke="currentColor"
-      //     fill="currentColor"
-      //     stroke-width="0" viewBox="0 0 24 24" width="20"><title>Remove</title><g id="Trash"><g><path d=
-      // "M19.45,4.06H15.27v-.5a1.5,1.5,0,0,0-1.5-1.5H10.23a1.5,1.5,0,0,0-1.5,1.5v.5H4.55a.5.5,0,0,0,0,1h.72l.42,14.45a2.493,2.493,0,0,0,2.5,2.43h7.62a2.493,2.493,0,0,0,2.5-2.43l.42-14.45h.72A.5.5,0,0,0,19.45,4.06Zm-9.72-.5a.5.5,0,0,1,.5-.5h3.54a.5.5,0,0,1,.5.5v.5H9.73Zm7.58,15.92a1.5,1.5,0,0,1-1.5,1.46H8.19a1.5,1.5,0,0,1-1.5-1.46L6.26,5.06H17.74Z"
-      //   ></path><path d="M8.375,8h0a.5.5,0,0,1,1,0l.25,10a.5.5,0,0,1-1,0Z"></path><path d=
-      // "M15.625,8.007a.5.5,0,0,0-1,0h0l-.25,10a.5.5,0,0,0,1,0Z"></path></g></g></svg>
 
   Btn = ({ind=0}) => <div><Button id='adb' className='mwu atb cb'
     ><svg height="22" viewBox="8 0 8 24" fill="none" stroke="currentColor"
@@ -253,7 +232,28 @@ export default function Home() {
       className="lucide lucide-ellipsis-vertical">
         <circle cx="12" cy="12" r="1"/><circle cx="12" cy="5" r="1"/><circle cx="12" cy="19" r="1"/>
     </svg><div className="StyledWidgetMenu--1ypwjps bYKxqt">
-  </div></Button><Button>hello</Button></div>,
+  </div></Button><Button id='del'
+    onClick={ (el : any) => { let arts = artists, c = (el.target as HTMLElement).classList, inde=0;
+          nextId-=15
+          for (let i = 0; i < c.length; i++) { if (c[i].match(/c\d/)) {
+            // console.log(c[i])
+          inde = +c[i].substring(1); break } }
+      artists.forEach((e:any) => { e.splice(inde, 1) // remove 1 item only
+      } ); tr = document.querySelector('#scrw.r')
+      // Variant
+      tr?.append(relm(<Col xs lg="2" className="df justify-content-md-center"
+        style={{ width: '100%' }}>
+          <Card id='fg1' isFooterBlurred radius="lg" className="br2 cb p-1 border-none"
+      >Variant { co+1 }</Card></Col>, ['w3', 'df', 'top'] ) )
+      // console.log(b, relm(k))"c"+{ind}+
+      setArtists(arts); todo('Variant', 'removed!') } }
+          className={"c"+ind+" mwu self-center"}>
+      <svg id='sdel' className="feather feather-x-circle" stroke="currentColor"
+        fill="currentColor"
+    stroke-width="0" viewBox="0 0 24 24" width="20"><title>Remove</title><g id="Trash"><g><path d=
+  "M19.45,4.06H15.27v-.5a1.5,1.5,0,0,0-1.5-1.5H10.23a1.5,1.5,0,0,0-1.5,1.5v.5H4.55a.5.5,0,0,0,0,1h.72l.42,14.45a2.493,2.493,0,0,0,2.5,2.43h7.62a2.493,2.493,0,0,0,2.5-2.43l.42-14.45h.72A.5.5,0,0,0,19.45,4.06Zm-9.72-.5a.5.5,0,0,1,.5-.5h3.54a.5.5,0,0,1,.5.5v.5H9.73Zm7.58,15.92a1.5,1.5,0,0,1-1.5,1.46H8.19a1.5,1.5,0,0,1-1.5-1.46L6.26,5.06H17.74Z"
+    ></path><path d="M8.375,8h0a.5.5,0,0,1,1,0l.25,10a.5.5,0,0,1-1,0Z"></path><path d=
+  "M15.625,8.007a.5.5,0,0,0-1,0h0l-.25,10a.5.5,0,0,0,1,0Z"></path></g></g></svg></Button></div>,
 
   rows = (i=0) => <Row id='scrw' className={ 'r'+i+' h17 ac' } style={{ width: nextId+'vw' }}
     >{ imco(i) }<ul className='df fon px-0'>
