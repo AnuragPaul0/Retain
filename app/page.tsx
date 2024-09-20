d2e24bb9f65b390c1440de86e09ce38bcf883ef6
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-09-20 13:04:54 +0530

    hsl(var(--nextui-default)/.4)

--- app/page.tsx
+++ app/page.tsx
@@ -630,11 +630,11 @@ export default function Home() {
               height="20px"
               version="1.1" id="Capa_1" viewBox="0 0 450.4 450.4" xmlSpace="preserve"><path d=
  "M484.1,454.796l-110.5-110.6c29.8-36.3,47.6-82.8,47.6-133.4c0-116.3-94.3-210.6-210.6-210.6S0,94.496,0,210.796   s94.3,210.6,210.6,210.6c50.8,0,97.4-18,133.8-48l110.5,110.5c12.9,11.8,25,4.2,29.2,0C492.5,475.596,492.5,463.096,484.1,454.796z    M41.1,210.796c0-93.6,75.9-169.5,169.5-169.5s169.6,75.9,169.6,169.5s-75.9,169.5-169.5,169.5S41.1,304.396,41.1,210.796z"/>
-              </svg></span>
-              <input id='bl0' onKeyUp={input} type="text" className="form-control" placeholder="Search"
+            </svg></span>
+            <input id='bl0' onKeyUp={input} type="text" className="form-control" placeholder="Search"
         aria-label="Search" aria-describedby="basic-addon1"/></div></form></nav>
-        </div><Button onClick={ () => myFunction() } id='buno' className=
-          {'action-label mwu close h-auto z-10 shadow-medium self-center p-2'+
+        </div><Button onClick={ () => myFunction() } className=
+          {'buno action-label mwu close h-auto z-10 shadow-medium self-center p-2'+
           ' top-0 radius-large' } variant="flat" color="default" radius="lg" size="sm">
         <span>&times;</span></Button>
 
