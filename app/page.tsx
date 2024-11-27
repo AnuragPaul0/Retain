b2297093422b830e7e5d4a194d7429b2d7178326
Anurag Paul <20ec01045@iitbbs.ac.in>
2024-11-27 20:57:46 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -382,14 +382,14 @@ export default function Home() {
     characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()-+=[]{}|;:,./?'.split(''), 
     e = document.querySelector('#gre'),
     t = 'Publish Feed'
-    e.addEventListener('mouseover', () => {
+    if (e) { e.addEventListener('mouseover', () => {
       // console.log('in')
       interval = setInterval(() => { k = ''; t.split('').forEach((element: any) => {
       // console.log(element)
       k+=characters[randomInteger(0, characters.length - 1)] })
       e.innerHTML = k
         }, 50) });
-    e.addEventListener('mouseout', () => { clearInterval(interval); e.innerHTML = 'Publish Feed'})
+    e.addEventListener('mouseout', () => { clearInterval(interval); e.innerHTML = 'Publish Feed'}) }
     }
   
     useEffect(() => { addEventListeners()
